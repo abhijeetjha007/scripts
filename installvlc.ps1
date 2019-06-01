f55a03fc45c7c08958dd7b9b5ecdfc6bacbdd87e
abhijeetjha007 <34881085+abhijeetjha007@users.noreply.github.com>
2019-06-01 16:32:14 +0530

    Delete installvlc.ps1

--- installvlc.ps1
+++
@@ -1,4 +0,0 @@
-﻿$source = "https://mirrors.estointernet.in/videolan/vlc/3.0.6/win64/vlc-3.0.6-win64.exe"
-$destination = "C:\Downloads" + $source.Split('/')[-1]
-
-Start-Process -FilePath "$destination" -ArgumentList "/S","/v","/qn" -Wait
