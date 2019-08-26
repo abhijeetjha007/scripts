40e5f275de6210b66441b11a8e98ef771c01d0c7
abhijeetjha007 <34881085+abhijeetjha007@users.noreply.github.com>
2019-08-26 14:50:04 +0530

    Create real.sh

---
+++ real.sh
@@ -0,0 +1,5 @@
+# apt-get install curl
+# yum install curl
+# yum install redhat-lsb redhat-lsb-core redhat-lsb-desktop
+# curl http://client-software.real.com/free/unix/RealPlayer11GOLD.rpm
+# rpm -ivh RealPlayer11GOLD.rpm
